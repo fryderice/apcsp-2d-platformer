01f92b093408645157b9a4972381fb90b282b045
fryderice <ryder.ruiz@student.csd509j.net>
2026-02-17 08:53:19 -0800

    Added a player and controls, and a basic template for a map

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
- 
+ let mySprite = sprites.create(img`
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 f f 3 3 3 3
+     3 3 3 f f 3 3 3 3 3 f f 3 3 3 3
+     3 3 3 f f 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 f 3 3 3 3 3 3 3 3 3 f 3 3 3
+     3 3 f f 3 3 3 3 3 3 f f f 3 3 3
+     3 3 3 f f f f f f f f 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+ `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 14))
+mySprite.vy = 200;
