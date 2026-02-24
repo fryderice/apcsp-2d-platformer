c38c5fac990e36edc15e4b129a9712226406bd41
Oskar-Heath-Kim <oskar.heath-kim@student.csd509j.net>
2026-02-24 08:43:47 -0800

    creates  the umbrella sprite!

--- main.ts
+++ main.ts
@@ -1,23 +1,6 @@
 //Global 
 
-let mySprite = sprites.create(img`
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 f f 2 2 2 2
-     2 2 2 f f 2 2 2 2 2 f f 2 2 2 2
-     2 2 2 f f 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 f 2 2 2 2 2 2 2 2 2 f 2 2 2
-     2 2 f f 2 2 2 2 2 2 f f f 2 2 2
-     2 2 2 f f f f f f f f 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
- `, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`umbrella`, SpriteKind.Player)
 
 let level = 0
 
