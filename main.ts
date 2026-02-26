d5d75d99a4dc59b648d3c7c5ddf1526ff4dda670
fryderice <ryder.ruiz@student.csd509j.net>
2026-02-26 09:20:15 -0800

    Added levels and a level change system

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 //Global 
 
 let mySprite = sprites.create(assets.image`umbrella`, SpriteKind.Player)
+
 let level = 0
 
-let x = "x"
-let y = "y"
-
 //Constants
+
+let levels = [tilemap`levelOne`, tilemap`levelTwo`, tilemap`levelThree`]
 
 //Functions
 
@@ -15,6 +15,79 @@ function createMushroom (){
     mushroom.ay = 200
     mushroom.vx = 0
 }
+
+function setNextLevel (){
+    currentLevel++;
+    tiles.setCurrentTilemap(levels[currentLevel]);
+    tiles.placeOnRandomTile(mySprite, assets.tile`spawnPoint`)
+}
+
+//Event Handlers
+
+scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
+    // If the mushroom hits the floor (bottom collision)
+    if (sprite.isHittingTile(CollisionDirection.Bottom)) {
+        // If it isn't moving yet, start sprinting right
+        if (sprite.vx == 0) {
+            sprite.vx = -60
+        }
+    }
+
+    if (sprite.isHittingTile(CollisionDirection.Left)) {
+        sprite.destroy(effects.disintegrate)
+    }
+})
+
+// player jumps with up (W on keyboard), and floats with A (space on keyboard)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -125
+    }
+})
+controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
+    if (!(mySprite.isHittingTile(CollisionDirection.Bottom)) && controller.A.isPressed()) {
+        mySprite.vy = 0;
+    }
+})
+
+scene.onOverlapTile(SpriteKind.Player, assets.tile`lava`, function (sprite, location) {
+    // Visual feedback
+    sprite.startEffect(effects.fire, 200)
+    scene.cameraShake(4, 500)
+    
+    // Consequence: Lose a life and reset position
+    info.changeLifeBy(-1)
+    
+    // Optional: Move player back to start so they don't instantly die again
+    //tiles.placeOnRandomTile(sprite, assets.tile`startNode`)
+})
+
+scene.onOverlapTile(SpriteKind.Player, assets.tile`flag`, function(sprite: Sprite, location: tiles.Location) {
+    setNextLevel();
+})
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(sprite: Sprite, otherSprite: Sprite) {
+    otherSprite.destroy(effects.disintegrate)
+    info.changeLifeBy(-1)
+})
+
+//Main
+
+createMushroom()
+info.setLife(3)
+controller.moveSprite(mySprite, 100, 0)
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 14))
+mySprite.ay = 200;
+//Global 
+
+let x = "x"
+let y = "y"
+
+//Constants
+
+//Functions
 
 function newCheckPoint (posType:string){
     if(posType == "x"){ //this must be manually coded, because for each level, you're gonna have to specify which flag is the checkpoint
@@ -68,13 +141,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`lava`, function (sprite, loca
     //tiles.placeOnRandomTile(sprite, assets.tile`startNode`)
 })
 
-scene.onOverlapTile(SpriteKind.Player, assets.tile`flag`, function(sprite: Sprite, location: tiles.Location) {
-    level++
-    if(level == 1){
-        tiles.setCurrentTilemap(tilemap`level2`)
-    }
-})
-
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(sprite: Sprite, otherSprite: Sprite) {
     otherSprite.destroy(effects.disintegrate)
     info.changeLifeBy(-1)
@@ -89,8 +155,10 @@ info.onLifeZero(function() {
 
 createMushroom()
 info.setLife(3)
-controller.moveSprite(mySprite, 100, 0)
-tiles.setCurrentTilemap(tilemap`level1`)
+controller.moveSprite(mySprite, 75, 0)
+tiles.setCurrentTilemap(levels[0])
 scene.cameraFollowSprite(mySprite)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 14))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 12))
 mySprite.ay = 200;
+scene.setBackgroundColor(9)
+let currentLevel = 0;
